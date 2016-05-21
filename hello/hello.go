244863ced10fdabb8982c76071cbd8e8e95f4384
Bradley Harrison <bradleyharrison@Bradleys-iMac.local>
2016-05-21 13:55:51 -0400

    adding user stuffen

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/user/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("Hello, world.\n"))
+        fmt.Printf(stringutil.Reverse("RaceCar, Madam.\n")) 
+
+}
